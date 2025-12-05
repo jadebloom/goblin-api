c88e850efa7ae0a170fe0f122f06fce1867765f8
jadebloom <warfacemeister@mail.ru>
2025-12-04 23:45:45 -0500

    init: initialized the project
    Added Spring Boot project with dependencies

---
+++ src/test/java/com/jadebloom/goblin_api/GoblinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadebloom.goblin_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoblinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
