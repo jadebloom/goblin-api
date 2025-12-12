64892030c1b04f2175475fc3fd22b22e390fccff
jadebloom <warfacemeister@mail.ru>
2025-12-12 01:44:26 -0500

    style: removed useless import

--- src/main/java/com/jadebloom/goblin_api/currency/validation/ValidCurrencyName.java
+++ src/main/java/com/jadebloom/goblin_api/currency/validation/ValidCurrencyName.java
@@ -7,7 +7,6 @@ import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
