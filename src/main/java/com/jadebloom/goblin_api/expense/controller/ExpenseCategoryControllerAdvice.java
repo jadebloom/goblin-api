47a24b5ae749c140581269b9e6a8fe47f799c473
jadebloom <warfacemeister@mail.ru>
2025-12-15 21:33:53 -0500

    refactor: removed unnecessary imports

--- src/main/java/com/jadebloom/goblin_api/expense/controller/ExpenseCategoryControllerAdvice.java
+++ src/main/java/com/jadebloom/goblin_api/expense/controller/ExpenseCategoryControllerAdvice.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.jadebloom.goblin_api.currency.error.CurrencyInUseException;
 import com.jadebloom.goblin_api.expense.error.ExpenseCategoryInUseException;
 import com.jadebloom.goblin_api.expense.error.ExpenseCategoryNameUnavailableException;
 import com.jadebloom.goblin_api.expense.error.ExpenseCategoryNotFoundException;
