3f4c284304f4343d6b6917496add036b87b00946
jadebloom <warfacemeister@mail.ru>
2025-12-23 20:31:45 -0500

    feat: added missing nullable=false in CurrencyEntity

--- src/main/java/com/jadebloom/goblin_api/currency/entity/CurrencyEntity.java
+++ src/main/java/com/jadebloom/goblin_api/currency/entity/CurrencyEntity.java
@@ -23,7 +23,7 @@ public class CurrencyEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(updatable = false)
+    @Column(nullable = false, updatable = false)
     private Long id;
 
     @Column(unique = true, nullable = false, length = 64)
@@ -35,7 +35,7 @@ public class CurrencyEntity {
     private String alphabeticalCode;
 
     @CreationTimestamp
-    @Column(name = "created_at", updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private ZonedDateTime createdAt;
 
     @ManyToOne
