dc79dffed70adef92d535ee3b076443ccafd3755
jadebloom <warfacemeister@mail.ru>
2025-12-30 15:19:39 -0500

    feat: updted SecurityContextUtils

--- src/main/java/com/jadebloom/goblin_api/security/util/SecurityContextUtils.java
+++ src/main/java/com/jadebloom/goblin_api/security/util/SecurityContextUtils.java
@@ -4,17 +4,28 @@ import java.util.Optional;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 
 public class SecurityContextUtils {
 
     public static Optional<String> getAuthenticatedUserEmail() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        if (auth == null || !(auth.getPrincipal() instanceof String)) {
+        if (auth == null || auth.getPrincipal() == null) {
             return Optional.empty();
         }
 
-        return Optional.of((String) auth.getPrincipal());
+        if (auth.getPrincipal() instanceof String) {
+            return Optional.of((String) auth.getPrincipal());
+        }
+
+        if (auth.getPrincipal() instanceof User) {
+            User user = (User) auth.getPrincipal();
+
+            return Optional.of(user.getUsername());
+        }
+
+        return Optional.empty();
     }
 
 }
